d061224306ab512095e508141652c3a6a79ddc05
Anton Kukushkin <anthony.meloman@gmail.com>
2019-08-04 20:15:58 +0300

    Fixed FieldTest with new field (flexible size)

--- tests/io/hexlet/xo/model/FieldTest.java
+++ tests/io/hexlet/xo/model/FieldTest.java
@@ -11,14 +11,14 @@ public class FieldTest {
 
     @Test
     public void testGetSize() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
 
         assertEquals(3, field.getSize());
     }
 
     @Test
     public void testSetFigure() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, 0);
         final Figure inputFigure = Figure.X;
 
@@ -30,7 +30,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenFigureNotSet() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, 0);
 
         final Figure actualFigure = field.getFigure(inputPoint);
@@ -40,7 +40,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenXIsLessThanZero() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(-1, 0);
 
         try {
@@ -51,7 +51,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenYIsLessThanZero() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, -1);
 
         try {
@@ -62,7 +62,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenXIsMoreThanZero() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(field.getSize() + 1, 0);
 
         try {
@@ -73,7 +73,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenYIsMoreThanZero() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, field.getSize() + 1);
 
         try {
